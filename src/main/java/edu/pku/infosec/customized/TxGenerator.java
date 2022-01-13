881e0156df3d85a0d1ac4aea491f0c1a4b8eea2b
LeoHLee <libaolinsy@163.com>
2022-01-14 04:57:23 +0800

    create util package

--- src/main/java/edu/pku/infosec/customized/TxGenerator.java
+++ src/main/java/edu/pku/infosec/customized/TxGenerator.java
@@ -7,10 +7,9 @@ import edu.pku.infosec.node.Node;
 import edu.pku.infosec.transaction.TxInfo;
 import edu.pku.infosec.transaction.TxStat;
 
-import java.util.Random;
+import edu.pku.infosec.util.RandomChoose;
 
 public class TxGenerator implements EventHandler {
-    private static final Random random = new Random();
     private final JSONObject inputNumDistribution;
     private final JSONObject outputNumDistribution;
     double DSAttackRate;
@@ -21,21 +20,11 @@ public class TxGenerator implements EventHandler {
         outputNumDistribution = txConfig.getJSONObject("outputNum");
     }
 
-    private static int chooseByDistribution(JSONObject distribution) {
-        double v = random.nextDouble();
-        for (String s : distribution.keySet()) {
-            v -= Double.parseDouble(s);
-            if (v <= 0)
-                return distribution.getInteger(s);
-        }
-        return 0; // not gonna happen
-    }
-
     @Override
     public void run(Node currentNode, EventParam param) {
         TxInfo tx = new TxInfo();
-        int inputNum = chooseByDistribution(inputNumDistribution);
-        tx.outputNum = chooseByDistribution(outputNumDistribution);
+        int inputNum = RandomChoose.chooseByDistribution(inputNumDistribution);
+        tx.outputNum = RandomChoose.chooseByDistribution(outputNumDistribution);
         for (int j = 0; j < inputNum; j++)
             tx.inputs.add(TxStat.getRandomUTXO()); // todo: double spending
     }
