e247c09088b4bc755caf8c2670baf5792f274ad0
LeoHLee <libaolinsy@163.com>
2022-01-16 17:51:34 +0800

    add a function to customize utxo initialization

--- src/main/java/edu/pku/infosec/customized/ModelData.java
+++ src/main/java/edu/pku/infosec/customized/ModelData.java
@@ -1,5 +1,12 @@
 package edu.pku.infosec.customized;
 
+import edu.pku.infosec.transaction.TxInput;
+
 public class ModelData {
     public static int nodeNum;
+
+    // addInitUTXO will be called at system initialization
+    public static void addInitUTXO(TxInput utxo) {
+
+    }
 }
